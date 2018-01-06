1fe7ffe26d40306e7e4e4ea643447b0debcb3a42
Vinod <vinodkumarpr@gmail.com>
2018-01-06 22:29:26 +0530

    Trello task listing

--- trlo/main.js
+++ trlo/main.js
@@ -14,22 +14,3 @@ loadTrelloTasks(function (err, tasks) {
         console.dir("Saved Trello tasks to " + file);
     });
 });
-
-/*
-var members = ["5892ccc5ac281eef7d257060","5892c951c100ed2efa13202b", "5952273ca10369e98383f35c"];
-
-members.forEach(id => {
-    var user = getUserInfo(id);
-    if (!user) {
-        requestUserInfo(id, (err, data) => {
-            if (err) {
-                console.dir("Error while loading " + id);
-            } else {
-                console.dir("[" + id + "]");
-            }
-            loaded = allUsersLoaded();
-            console.dir("All loaded " + loaded);
-        });
-    }
-});
-*/
