080a98be3ea283b8ecabd59da85527b797b19c2d
Vinod <vinodkumarpr@gmail.com>
2018-01-14 12:37:56 +0530

    Questions...

--- wrds/src/words/questions.js
+++ wrds/src/words/questions.js
@@ -1,2 +1,12 @@
 var Word = require("./word.js")
 
+class Questions {
+    constructor(wordlist){
+        this.wordlist = wordlist;
+    }
+    report(){
+        console.log("Questions are ...");
+    }
+}
+
+module.exports = Questions;
