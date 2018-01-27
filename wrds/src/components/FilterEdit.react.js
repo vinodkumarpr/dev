7604c4d16376b66739f2466b3e12aca11f569b6c
Vinod <vinodkumarpr@gmail.com>
2018-01-27 16:05:41 +0530

    HTML modified

--- wrds/src/components/FilterEdit.react.js
+++ wrds/src/components/FilterEdit.react.js
@@ -1,14 +1,25 @@
 var React = require('react');
 
+var itemTextStyle = {
+    "fontFamily": "Times New Roman, sans-serif"
+};
+
 var FilterEdit = React.createClass({
-    handleOnChange: function(event){
+    handleOnChange: function (event) {
         this.props.onApplyFilter(event.target.value);
     },
     render: function () {
         return (
-            <div className="input-group" style={{margin: '10px'}}>
-                <label className="control-label input-group-addon" htmlFor="filterInput">Search</label>
-                <input className="form-control" id="filterInput" type="text" onChange={this.handleOnChange}/>
+            <div className="container-fluid">
+                <div class="col-lg-6">
+                    <div className="input-group" style={{ margin: '20px' }}>
+                        <label className="control-label input-group-addon" htmlFor="filterInput" style={itemTextStyle}>Search</label>
+                        <input className="form-control" id="filterInput" type="text" onChange={this.handleOnChange} />
+                        <span className="input-group-btn">
+                            <button type="button" className="btn btn-primary">Quiz</button>
+                        </span>
+                    </div>
+                </div>
             </div>
         );
     }
--- wrds/src/components/WordList.react.js
+++ wrds/src/components/WordList.react.js
@@ -1,5 +1,10 @@
 var React = require('react');
 
+var itemTextStyle = {
+    "fontFamily": "Times New Roman, sans-serif" 
+};
+
+
 var WordList = React.createClass({
     handleClick: function (word) {
         if (this.props.onItemClick) {
@@ -17,15 +22,15 @@ var WordList = React.createClass({
         const listItems = this.props.words.map((word) => {
             if (this.props.selectedWord && word.word() == this.props.selectedWord.word()) {
                 return (
-                    <li className="list-group-item active" key={word.word()} onClick={this.handleClick.bind(this, word.word())}>{word.word()}</li>
+                    <li className="list-group-item active" style={itemTextStyle} key={word.word()} onClick={this.handleClick.bind(this, word.word())}>{word.word()}</li>
                 );
             }
-            return (<li className="list-group-item" key={word.word()} onClick={this.handleClick.bind(this, word.word())}>{word.word()}</li>
+            return (<li className="list-group-item" style={itemTextStyle} key={word.word()} onClick={this.handleClick.bind(this, word.word())}>{word.word()}</li>
             );
         });
 
         return (
-            <ul className="list-group">{listItems}</ul>
+            <ul className="list-group" style={{margin: '10px'}}>{listItems}</ul>
         );
     }
 });
