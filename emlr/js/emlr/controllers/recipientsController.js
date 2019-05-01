189dedb6fb161004d8dd7720dc1ac112e220eef2
vinodkumarpr <vinodkumarpr@gmail.com>
2019-05-01 16:05:59 +0530

    Added update and cancel buttons

--- emlr/js/emlr/controllers/recipientsController.js
+++ emlr/js/emlr/controllers/recipientsController.js
@@ -6,20 +6,25 @@
 
         var initialize = function () {
             recipientService.init(() => {
-                let list = recipientService.getRecipients();
-                $scope.table_list["columns"] = list.columns.slice(2);
-                $scope.table_list["rows"] = list.rows.map((row) => {
-                    return {
-                        "channel" : row.slice(0, 2),
-                        "recipients" : row.slice(2)
-                    }
-                });
+                loadRecipients()
             });
 
             var file_input = document.getElementById('file_input');
             file_input.addEventListener("change", function () {
                 $scope.loadCSV();
               });
+        }
+
+        function loadRecipients(){
+            let list = recipientService.getRecipients();
+            $scope.table_list["columns"] = list.columns.slice(2);
+            $scope.table_list["rows"] = list.rows.map((row) => {
+                return {
+                    "channel" : row.slice(0, 2),
+                    "recipients" : row.slice(2)
+                }
+            });
+            $scope.editing = false;
         }
 
         $scope.$on('recipients-display-updated', function(event, value) {
@@ -39,10 +44,18 @@
                         }
                     });    
                     $scope.editing = true;
+                    document.getElementById("file_input").value = null;
                 })
             });
         }
 
+        $scope.cancel = function (){
+            loadRecipients();
+        }
+
+        $scope.update = function (){
+
+        }
        initialize();
     };
     app.controller("recipientsController", ["$scope", "$http", "recipientService", recipientsController]);
